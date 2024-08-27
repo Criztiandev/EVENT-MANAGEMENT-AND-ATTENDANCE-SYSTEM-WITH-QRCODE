b1cdee46eedd0de14c7a42d061c54b2a97406c39
criztiandev <criztiandev@gmail.com>
2024-08-27 09:48:45 +0800

    Fixed something

--- src/views/admin/operator/pages/create.page.php
+++ src/views/admin/operator/pages/create.page.php
@@ -106,9 +106,7 @@ require from("views/helper/partials/sidebar.partials.php");
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             required>
                             <option disabled selected>Select your Organization</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
+                            <option value="CASCSC">CASCSC</option>
                         </select>
                     </div>
 
@@ -120,9 +118,7 @@ require from("views/helper/partials/sidebar.partials.php");
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             required>
                             <option disabled selected>Select your Position</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
+                            <option value="President">President</option>
                         </select>
                     </div>
 
