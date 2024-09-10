6f6a6ac52ecebe3a2260b17cc6b564b047f35610
criztiandev <criztiandev@gmail.com>
2024-09-10 08:01:19 +0800

    Updated error location

--- src/views/admin/event/pages/update.page.php
+++ src/views/admin/event/pages/update.page.php
@@ -67,7 +67,7 @@ require from("views/helper/partials/sidebar.partials.php");
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
                         <input type="location" name="LOCATION" id="LOCATION"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="Enter your location" value="<?= $details["LOCATION"] ?> required="" />
+                            placeholder="Enter your location" value="<?= $details["LOCATION"] ?>" required="" />
                     </div>
 
                     <div></div>
